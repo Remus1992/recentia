9dbd590a492a8353573af1c9e3140a88f17f391a
Remus1992 <Remington.Henderson@gmail.com>
2018-11-12 17:52:45 -0800

    Sending 'Ingredients'

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../HOC/Auxilary';
+import classes from './Layout.css'
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDraw, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
