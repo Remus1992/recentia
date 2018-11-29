b3b472c9d7a36c5597f2b8da60ab831a73d15aa3
Remus1992 <Remington.Henderson@gmail.com>
2018-11-28 16:32:42 -0800

    Sending Updates:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import Layout from './HOC/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/CheckOut/CheckOut';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
     render() {
@@ -11,6 +12,7 @@ class App extends Component {
                 <Layout>
                     <Switch>
                         <Route path='/checkout' component={Checkout}/>
+                        <Route path='/orders' component={Orders}/>
                         {/* Remember that 'exact' can be used instead of switch */}
                         <Route path='/' component={BurgerBuilder}/>
                     </Switch>
