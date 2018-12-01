89c31386183210984b74d9149914f4ad057190c5
Remus1992 <Remington.Henderson@gmail.com>
2018-12-01 08:06:13 -0800

    Sending Sub Routes

--- src/containers/RecentiaClinicalLibrary/RecentiaClinicalLibrary.js
+++ src/containers/RecentiaClinicalLibrary/RecentiaClinicalLibrary.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
-import LibraryToolbar from '../../components/LibraryToolbar/LibraryToolbar';
-
+import { Route, NavLink, Switch } from 'react-router-dom';
 // import axios from '../../../axios-orders';
 
+// Components
+import LibraryToolbar from '../../components/LibraryToolbar/LibraryToolbar';
 import Profile from '../../components/Profile/Profile';
 import Header from '../../components/Header/Header';
 
+// CSS
 import classes from './RecentiaClinicalLibrary.css'
+
+// Sub-Containers
+import Dictionary from './Dictionary/Dictionary';
+import Metathesaurus from './Metathesaurus/Metathesaurus';
+import CodingSystems from './CodingSystems/CodingSystems';
+import ValueSets from './ValueSets/ValueSets';
+import Languages from './Languages/Languages';
 
 class recentiaClinicalLibrary extends Component {
     state = {
         definitions: null
     };
-
-
 
     render() {
         return (
@@ -34,6 +41,13 @@ class recentiaClinicalLibrary extends Component {
                     </div>
                     <LibraryToolbar/>
                 </div>
+                <Switch>
+                    <Route path={this.props.match.url + "/dictionary" } component={Dictionary}/>
+                    <Route path={this.props.match.url + "/metathesaurus"} component={Metathesaurus}/>
+                    <Route path={this.props.match.url + "/coding_systems"} component={CodingSystems}/>
+                    <Route path={this.props.match.url + "/value_sets"} component={ValueSets}/>
+                    <Route path={this.props.match.url + "/languages"} component={Languages}/>
+                </Switch>
             </React.Fragment>
         );
     }
