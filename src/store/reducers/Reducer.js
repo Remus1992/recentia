d2e6ba491c88c84e2b1ddbbe9011e6711c6d3690
Remus1992 <Remington.Henderson@gmail.com>
2019-01-17 15:47:10 -0800

    Sending Updates

--- src/store/reducers/Reducer.js
+++ src/store/reducers/Reducer.js
@@ -20,27 +20,27 @@ import session_id, {
 
 
 const initialState = {
-    search_term = search_term,
-    query_type = query_type,
-    input_language = input_language,
-    output_language = output_language,
-    input_vocabulary = input_vocabulary,
-    output_vocabulary = output_vocabulary,
-    semantic_type = semantic_type,
-    clinical_variable = clinical_variable,
-    filter_by_value_sets = filter_by_value_sets,
-    sort_order = sort_order,
-    row_off_set_arg = row_off_set_arg,
-    rows_in_subset_arg = rows_in_subset_arg,
-    ip_address = ip_address,
-    clinic_license = clinic_license,
-    physician_license = physician_license
+    search_term: search_term,
+    query_type: query_type,
+    input_language: input_language,
+    output_language: output_language,
+    input_vocabulary: input_vocabulary,
+    output_vocabulary: output_vocabulary,
+    semantic_type: semantic_type,
+    clinical_variable: clinical_variable,
+    filter_by_value_sets: filter_by_value_sets,
+    sort_order: sort_order,
+    row_off_set_arg: row_off_set_arg,
+    rows_in_subset_arg: rows_in_subset_arg,
+    ip_address: ip_address,
+    clinic_license: clinic_license,
+    physician_license: physician_license
 };
 
 const getSearchTerm = (state, action) => {
     const updatedSearchTerm = {[action.searchTerm]};
     return
-}
+};
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
