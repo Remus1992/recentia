7fb9d06a0cec45d4c5f70db96a713b5e023177c7
Remus1992 <Remington.Henderson@gmail.com>
2019-01-18 19:41:41 -0800

    Sending Updates

--- src/containers/RecentiaClinicalLibrary/Metathesaurus/Metathesaurus.js
+++ src/containers/RecentiaClinicalLibrary/Metathesaurus/Metathesaurus.js
@@ -27,63 +27,62 @@ import session_id, {
 
 // import classes from './Metathesaurus.css'
 
-// 222222
 // export function updateText(text) {
 //     this.setState({text})
 // }
-//
-// 2222222
-// export function handleInputChange(query) {
-//         this.setState({
-//             query: query
-//         }, () => {
-//             console.log(this.state.query);
-//             if (this.state.query && this.state.query.length > 1) {
-//                 if (this.state.query.length % 2 === 0) {
-//                     getInfo()
-//                 }
-//             } else if (!this.state.query) {
-//                 console.log('Unsuccessful')
-//             }
-//         })
-//     }
+
+export function handleInputChange(query) {
+    this.setState({
+        query: query
+    }, () => {
+        console.log(this.state.query);
+        if (this.state.query && this.state.query.length > 1) {
+            if (this.state.query.length % 2 === 0) {
+                getInfo()
+            }
+        } else if (!this.state.query) {
+            console.log('Unsuccessful')
+        }
+    })
+}
 
 export function getInfo() {
-        // console.log(this.props);
-        axios.get('/getTerms.php?SessionID=' + session_id
-            // + '&SearchTerm=' + search_term
-            + '&SearchTerm=' + this.state.query
-            + '&QueryType=' + query_type
-            + '&InputLanguage=' + input_language
-            + '&OutputLanguage=' + output_language
-            + '&InputVocabulary=' + input_vocabulary
-            + '&OutputVocabulary=' + output_vocabulary
-            + '&SemanticType=' + semantic_type
-            + '&ClinicalVariable=' + clinical_variable
-            + '&FilterByValueSets=' + filter_by_value_sets
-            + '&SortOrder=' + sort_order
-            + '&RowOffsetArg=' + row_off_set_arg
-            + '&RowsInSubsetArg=' + rows_in_subset_arg
-            + '&IP_Address=' + ip_address
-            + '&ClinicLicense=' + clinic_license
-            + '&PhysicianLicense=' + physician_license
-        )
-            .then(response => {
-                const getTermItems = response.data;
-                this.setState({getTermItems: getTermItems});
-                console.log(response);
+    // console.log(this.props);
+    axios.get('/getTerms.php?SessionID=' + session_id
+        // + '&SearchTerm=' + search_term
+        + '&SearchTerm=' + this.state.query
+        + '&QueryType=' + query_type
+        + '&InputLanguage=' + input_language
+        + '&OutputLanguage=' + output_language
+        + '&InputVocabulary=' + input_vocabulary
+        + '&OutputVocabulary=' + output_vocabulary
+        + '&SemanticType=' + semantic_type
+        + '&ClinicalVariable=' + clinical_variable
+        + '&FilterByValueSets=' + filter_by_value_sets
+        + '&SortOrder=' + sort_order
+        + '&RowOffsetArg=' + row_off_set_arg
+        + '&RowsInSubsetArg=' + rows_in_subset_arg
+        + '&IP_Address=' + ip_address
+        + '&ClinicLicense=' + clinic_license
+        + '&PhysicianLicense=' + physician_license
+    )
+        .then(response => {
+            const getTermItems = response.data;
+            this.setState({getTermItems: getTermItems});
+            console.log(response);
 
-                let i = getTermItems.length;
-                console.log(i);
-                for (let index = 0; index < getTermItems.length; index++) {
-                    console.log("Concept is: " + getTermItems[index]["Concept"])
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                // this.setState({error: true});
-            });
-    }
+            let i = getTermItems.length;
+            console.log(i);
+            for (let index = 0; index < getTermItems.length; index++) {
+                console.log("Concept is: " + getTermItems[index]["Concept"])
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            // this.setState({error: true});
+        });
+}
+
 
 class Metathesaurus extends Component {
     // state = {
@@ -96,64 +95,29 @@ class Metathesaurus extends Component {
         this.state = {
             text: 'Asthma',
             getTermItems: [],
-            query: ''
+            query: '',
+
+            searchTermLocal: '',
+            currentlyDisplayed: this.props.searchTerm
         };
+
+        // bindings
+        this.onInputChange = this.onInputChange.bind(this)
+
         // 222222
         // updateText = updateText.bind(this);
         // handleInputChange = handleInputChange.bind(this);
         // getInfo = getInfo.bind(this);
-
     }
 
-    // getInfo  = () => {
-    //     console.log(this.props);
-    //     axios.get('/getTerms.php?SessionID=' + session_id
-    //         // + '&SearchTerm=' + search_term
-    //         + '&SearchTerm=' + this.state.text
-    //         + '&QueryType=' + query_type
-    //         + '&InputLanguage=' + input_language
-    //         + '&OutputLanguage=' + output_language
-    //         + '&InputVocabulary=' + input_vocabulary
-    //         + '&OutputVocabulary=' + output_vocabulary
-    //         + '&SemanticType=' + semantic_type
-    //         + '&ClinicalVariable=' + clinical_variable
-    //         + '&FilterByValueSets=' + filter_by_value_sets
-    //         + '&SortOrder=' + sort_order
-    //         + '&RowOffsetArg=' + row_off_set_arg
-    //         + '&RowsInSubsetArg=' + rows_in_subset_arg
-    //         + '&IP_Address=' + ip_address
-    //         + '&ClinicLicense=' + clinic_license
-    //         + '&PhysicianLicense=' + physician_license
-    //     )
-    //         .then(response => {
-    //             const getTermItems = response.data;
-    //             this.setState({getTermItems: getTermItems});
-    //             console.log(response);
-    //
-    //             let i = getTermItems.length;
-    //             console.log(i);
-    //             for (let index = 0; index < getTermItems.length; index++) {
-    //                 console.log("Concept is: " + getTermItems[index]["Concept"])
-    //             }
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //             // this.setState({error: true});
-    //         });
-    // };
+    onInputChange(event) {
+        // let newlyDisplayed = ();
 
-    // handleInputChange = () => {
-    //     this.setState({
-    //         query: this.search.value
-    //     }, () => {
-    //         if (this.state.query && this.state.query.length > 1) {
-    //             if (this.state.query.length % 2 === 0) {
-    //                 this.getInfo()
-    //             }
-    //         } else if (!this.state.query) {
-    //         }
-    //     })
-    // };
+        this.setState({
+            searchTerm: event.target.value,
+            // currentlyDisplayed: newlyDisplayed
+        })
+    }
 
 
     render() {
