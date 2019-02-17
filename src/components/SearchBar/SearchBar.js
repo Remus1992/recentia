208458a0a8022fd11e78ff6fa10c360dde09ad34
Remus1992 <Remington.Henderson@gmail.com>
2019-02-16 17:09:32 -0800

    Sending Material UI Updates

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import classesSecondary from "./SearchBar.css";
 import {connect} from 'react-redux';
-// import {withStyles} from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+import grey from '@material-ui/core/colors/grey';
+import Icon from '@material-ui/core/Icon';
 
 import * as actions from '../../store/actions/index';
 
@@ -24,6 +26,28 @@ const theme = createMuiTheme({
             }
         }
     }
+});
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+    iconClicked: {
+        margin: theme.spacing.unit * 2,
+        color: grey[400],
+        '&:hover': {
+            color: grey[800],
+        },
+    },
+    iconHover: {
+        margin: theme.spacing.unit * 2,
+        color: grey[100],
+        '&:hover': {
+            color: grey[800],
+        },
+    },
 });
 
 
@@ -56,6 +80,8 @@ class SearchBar extends Component {
     }
 
     render() {
+        const {classes} = this.props;
+
         return (
             <MuiThemeProvider theme={theme}>
                 <div className={classesSecondary.recentia_search_wrapper}>
@@ -65,23 +91,29 @@ class SearchBar extends Component {
                         {/*placeholder="Search"*/}
                         {/*onChange={(e) => this.props.onSearchChange(e.target.value)}*/}
                         {/*className={classesSecondary.search_bar}/>*/}
-                        <TextField
-                            placeholder="Search..."
-                            // className={classes.input}
-                            style={{margin: 8}}
-                            fullWidth
-                            margin="normal"
-                            onChange={(e) => this.props.onSearchChange(e.target.value)}
-                            // InputProps={{
-                            //     className: classes.input,
-                            // }}
-                        />
+                        <div className={classes.root}>
+                            <TextField
+                                placeholder="Search..."
+                                // className={classes.input}
+                                style={{margin: 8}}
+                                fullWidth
+                                margin="normal"
+                                onChange={(e) => this.props.onSearchChange(e.target.value)}
+                                // InputProps={{
+                                //     className: classes.input,
+                                // }}
+                            />
+                            <Icon className={this.state.expanded ?  classes.iconClicked : classes.iconHover} onClick={(e) => this.handleClick(e)}>
+                                language
+                            </Icon>
+                        </div>
                         {/*<button*/}
                         {/*className={this.state.expanded ? [classesSecondary.accordion, classesSecondary.active].join(' ') : classesSecondary.accordion}*/}
                         {/*onClick={(e) => this.handleClick(e)}>Filter</button>*/}
-                        {/*<div className={classesSecondary.panel} style={{maxHeight: this.state.expanded ? '150px' : null}}>*/}
-                        {/*<p>Translate</p>*/}
-                        {/*</div>*/}
+                        <div className={classesSecondary.panel}
+                             style={{maxHeight: this.state.expanded ? '150px' : null}}>
+                            <p>Translate</p>
+                        </div>
 
                     </form>
                 </div>
@@ -97,5 +129,5 @@ const mapDispatchToProps = dispatch => {
     }
 };
 
-export default connect(null, mapDispatchToProps)(SearchBar);
-// export default connect(null, mapDispatchToProps)(withStyles(styles)(SearchBar));
+// export default connect(null, mapDispatchToProps)(SearchBar);
+export default connect(null, mapDispatchToProps)(withStyles(styles)(SearchBar));
